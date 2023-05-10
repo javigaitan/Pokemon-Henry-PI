6601dfeaa9a94d2ae67297432f3a11ba2f1f235b
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2023-05-10 00:41:44 -0300

    add Home and top bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router,Routes,Route, Link,} from "react-router-dom";
 import LandingPage from './Pages/Landing/LandingPage'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
 
           <Route exact path='/' element={<LandingPage/>} />
+          <Route exact path='/home' element={<Home/>} />
           
       </Routes>
       </div>
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './Home.css';
+import {Link} from "react-router-dom";
+
+
+
+export default function Home(){
+    return(
+        <div className="homebc">
+                <div className="top-bar">
+                    <Link to="/">
+                        <button className="button-back">Back</button>
+                    </Link>
+               
+                    <Link to="/">
+                        <button className="button-back">Create Pokémon</button>
+                    </Link>
+                </div>
+
+
+        </div>
+    )
+}
