818533ce470ad317fa46ea99cde98ddbc61cdc1a
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2023-05-19 01:49:30 -0300

    change icon public

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,21 @@ import {BrowserRouter as Router,Routes,Route, Link,} from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { GetPokemons, GetType } from "./actions/index";
-
-import LandingPage from './components/Landing/LandingPage'
-import Home from './components/Home/Home'
+import CreatePokemon from './components/CreatePokemon/CreatePokemon'
+import LandingPage from './components/Landing/LandingPage';
+import Home from './components/Home/Home';
+import {PokeDetail} from './components/PokeDetail/PokeDetail';
 
 function App() {
   return (
 
-    
-      
       <div className="App">
         <Routes>
 
           <Route exact path='/' element={<LandingPage/>} />
           <Route exact path='/home' element={<Home/>} />
+          <Route exact path="/pokemon/:id" element={<PokeDetail/>}/>
+          <Route exact path='/CreatePokemon' element={<CreatePokemon/>}/>
           
       </Routes>
       </div>
